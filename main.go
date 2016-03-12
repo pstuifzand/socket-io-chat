880fc0af9a681b24805f901f77a1642640b0b892
Peter Stuifzand <peter@stuifzand.eu>
2016-03-12 14:36:32 +0100

    Add Rooms and messages backend for #1

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
 	socketio "github.com/googollee/go-socket.io"
 	"log"
 	"math/rand"
 	"net/http"
+	"time"
 )
 
 type AdminMessage struct {
@@ -36,7 +38,38 @@ func RandStringRunes(n int) string {
 	return string(b)
 }
 
-func handleConnect(so socketio.Socket) {
+type ConnectHandler struct {
+	Backend Backend
+}
+
+type Backend interface {
+	AddRoom(room string) error
+	AddMessage(room string, msg string, name string) error
+}
+
+type RedisBackend struct {
+	Redis redis.Conn
+}
+
+func (backend *RedisBackend) AddRoom(room string) error {
+	backend.Redis.Do("SADD", "rooms", room)
+	return nil
+}
+
+func (backend *RedisBackend) AddMessage(room string, msg string, name string) error {
+	nextId, err := redis.Int64(backend.Redis.Do("INCR", "message:id"))
+	if err != nil {
+		return err
+	}
+	msgId := fmt.Sprintf("message:%d", nextId)
+	backend.Redis.Do("HMSET", msgId, "author", name, "room", room, "text", msg, "time", time.Now())
+	backend.Redis.Do("RPUSH", room+":messages", msgId)
+	return nil
+}
+
+func (handler *ConnectHandler) handleConnect(so socketio.Socket) {
+	log.Println("Handler connect")
+
 	so.On("admin", func(msg AdminMessage) {
 		log.Printf("admin: %s\n", msg.Key)
 		names[so.Id()] = "Admin"
@@ -49,10 +82,11 @@ func handleConnect(so socketio.Socket) {
 		roomId := msg.Room
 		if msg.Room == "" {
 			key := RandStringRunes(8)
-			roomId = "room-" + key
+			roomId = "room:" + key
 		}
 		m := make(map[string]interface{})
 		m["Room"] = roomId
+		handler.Backend.AddRoom(roomId)
 		so.Join(roomId)
 		so.BroadcastTo("admins", "new_room", m)
 		so.Emit("new_room", m)
@@ -68,6 +102,7 @@ func handleConnect(so socketio.Socket) {
 		log.Printf("Message received for room %s: %s\n",
 			msg.Room, msg.Msg)
 		msg.Name = names[so.Id()]
+		handler.Backend.AddMessage(msg.Room, msg.Name, msg.Msg)
 		so.BroadcastTo(msg.Room, "message", msg)
 		so.Emit("message", msg)
 	})
@@ -76,13 +111,20 @@ func handleConnect(so socketio.Socket) {
 var names map[string]string
 
 func main() {
+	conn, err := redis.Dial("tcp", ":7777")
+	if err != nil {
+		// handle error
+	}
+	defer conn.Close()
+
 	names = make(map[string]string)
 	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	server.On("connection", handleConnect)
-	server.On("reconnect", handleConnect)
+	connectHandler := &ConnectHandler{&RedisBackend{conn}}
+	server.On("connection", func(so socketio.Socket) { connectHandler.handleConnect(so) })
+	server.On("reconnection", func(so socketio.Socket) { connectHandler.handleConnect(so) })
 	server.On("error", func(so socketio.Socket, err error) {
 		log.Println("error:", err)
 	})
